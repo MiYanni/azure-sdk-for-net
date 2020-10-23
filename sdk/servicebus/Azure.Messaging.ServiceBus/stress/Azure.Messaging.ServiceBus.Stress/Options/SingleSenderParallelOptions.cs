887c528992e1f6c1acc82995463b7e8c54aeda95
Michael Yanni <MiYanni@microsoft.com>
2020-10-22 18:10:23 -0700

    Realized I made multiple senders instead of just 1 for SingleSenderParallelTest.

---
+++ sdk/servicebus/Azure.Messaging.ServiceBus/stress/Azure.Messaging.ServiceBus.Stress/Options/SingleSenderParallelOptions.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using CommandLine;
+
+namespace Azure.Messaging.ServiceBus.Stress.Options
+{
+    public class SingleSenderParallelOptions : ServiceBusStressOptions
+    {
+        [Option("sendDuration", Default = 10, HelpText = "Duration to continually send messages in seconds")]
+        public int SendDuration { get; set; }
+
+        [Option("sendDelay", Default = 1, HelpText = "Delay in-between sending messages in seconds")]
+        public int SendDelay { get; set; }
+
+        [Option("receiveDuration", Default = 3, HelpText = "Duration for the receiver to no longer gain new messages in seconds")]
+        public int ReceiveDuration { get; set; }
+
+        [Option("parallelSenders", Default = 3, HelpText = "Number of parallel senders to create")]
+        public int ParallelSenders { get; set; }
+    }
+}
---
+++ sdk/servicebus/Azure.Messaging.ServiceBus/stress/Azure.Messaging.ServiceBus.Stress/SingleSenderParallelTest.cs
@@ -0,0 +1,115 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Diagnostics;
+using System.Linq;
+using Azure.Test.Stress;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus.Stress.Metrics;
+using Azure.Messaging.ServiceBus.Stress.Options;
+
+namespace Azure.Messaging.ServiceBus.Stress
+{
+    public class SingleSenderParallelTest : StressTest<SingleSenderParallelOptions, SingleSenderMetrics>
+    {
+        public SingleSenderParallelTest(SingleSenderParallelOptions options, SingleSenderMetrics metrics)
+            : base(options, metrics)
+        {
+        }
+
+        public override async Task RunAsync(CancellationToken testDurationToken)
+        {
+#pragma warning disable AZC0100 // ConfigureAwait(false) must be used.
+            await using var client = new ServiceBusClient(Options.ConnectionString, new ServiceBusClientOptions
+            {
+                RetryOptions = new ServiceBusRetryOptions { TryTimeout = TimeSpan.FromSeconds(Options.TryTimeout) }
+            });
+#pragma warning restore AZC0100 // ConfigureAwait(false) must be used.
+
+            var sendDuration = TimeSpan.FromSeconds(Options.SendDuration);
+            while (!testDurationToken.IsCancellationRequested)
+            {
+                try
+                {
+                    var senders = Enumerable.Range(0, Options.ParallelSenders)
+                        .Select(_ => SendMessagesAsync(client, sendDuration, testDurationToken)).ToArray();
+
+                    await Task.WhenAll(senders).ConfigureAwait(false);
+
+                    await ReceiveAllMessagesAsync(client, testDurationToken).ConfigureAwait(false);
+                }
+                catch (Exception e) when (ContainsOperationCanceledException(e) && testDurationToken.IsCancellationRequested)
+                {
+                    // Ignore this exception as it is normal operation of the test for it to occur.
+                }
+                catch (Exception e)
+                {
+                    Metrics.Exceptions.Enqueue(e);
+                }
+            }
+        }
+
+        private async Task SendMessagesAsync(ServiceBusClient client, TimeSpan sendDuration, CancellationToken testDurationToken)
+        {
+#pragma warning disable AZC0100 // ConfigureAwait(false) must be used.
+            await using var sender = client.CreateSender(Options.QueueName);
+#pragma warning restore AZC0100 // ConfigureAwait(false) must be used.
+            var sendStopwatch = Stopwatch.StartNew();
+            while (sendStopwatch.Elapsed < sendDuration)
+            {
+                await sender.SendMessageAsync(new ServiceBusMessage(), testDurationToken).ConfigureAwait(false);
+                Metrics.IncrementSends();
+
+                await Task.Delay(TimeSpan.FromSeconds(Options.SendDelay), testDurationToken).ConfigureAwait(false);
+            }
+        }
+
+        private async Task ReceiveAllMessagesAsync(ServiceBusClient client, CancellationToken testDurationToken)
+        {
+#pragma warning disable AZC0100 // ConfigureAwait(false) must be used.
+            await using var processor = client.CreateProcessor(Options.QueueName);
+#pragma warning restore AZC0100 // ConfigureAwait(false) must be used.
+            processor.ProcessMessageAsync += MessageHandler;
+            processor.ProcessErrorAsync += ErrorHandler;
+
+            Task MessageHandler(ProcessMessageEventArgs args)
+            {
+                try
+                {
+                    var receivedMessage = args.Message;
+                    if (receivedMessage == null) return Task.CompletedTask;
+
+                    Metrics.IncrementReceives();
+                }
+                catch (Exception e) when (ContainsOperationCanceledException(e) && testDurationToken.IsCancellationRequested)
+                {
+                    // Ignore this exception as it is normal operation of the test for it to occur.
+                }
+                catch (Exception e)
+                {
+                    Metrics.Exceptions.Enqueue(e);
+                }
+
+                return Task.CompletedTask;
+            }
+
+            Task ErrorHandler(ProcessErrorEventArgs args)
+            {
+                if (!(ContainsOperationCanceledException(args.Exception) && testDurationToken.IsCancellationRequested))
+                {
+                    Metrics.Exceptions.Enqueue(args.Exception);
+                }
+
+                return Task.CompletedTask;
+            }
+
+            await processor.StartProcessingAsync(testDurationToken).ConfigureAwait(false);
+
+            await DelayUntil(() => !Metrics.IsActivelyReceiving(), TimeSpan.FromSeconds(Options.ReceiveDuration), testDurationToken).ConfigureAwait(false);
+
+            await processor.StopProcessingAsync(testDurationToken).ConfigureAwait(false);
+        }
+    }
+}
